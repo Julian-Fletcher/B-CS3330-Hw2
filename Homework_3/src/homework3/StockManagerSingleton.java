348955c5b25b8b86714d36229ccbcca6883f81ff
Ahmad Ennin <121472038+ARockEnnin@users.noreply.github.com>
2024-02-12 18:53:25 -0600

    Adding inventory.txt and initialize stock method
    Transfered the data from inventory.csv into a .txt file. Then began
    coding out initializeStock

--- Homework_3/src/homework3/StockManagerSingleton.java
+++ Homework_3/src/homework3/StockManagerSingleton.java
@@ -1,5 +1,40 @@
 package homework3;
 
-public class StockManagerSingleton {
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
+public class StockManagerSingleton 
+{
+	private static StockManagerSingleton instance = null;
+	private final static String inventoryFilePath = "inventory.txt";
+	
+	private StockManagerSingleton()
+	{
+	}
+	
+	public static StockManagerSingleton getInstance() 
+	{
+		if(instance == null)
+		{
+			instance = new StockManagerSingleton();
+		}
+		return instance;
+	}
+	
+	public boolean initializeStock()
+	{
+		try
+		{
+			Scanner fileIn = new Scanner(new FileInputStream(inventoryFilePath));
+		}
+		catch(FileNotFoundException e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
 }
+
